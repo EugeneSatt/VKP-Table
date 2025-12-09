08383b0bd742949670b9b3f7a33fd3e785e8e0f4
EugeneSatt <vrtx.prod@gmail.com>
2025-12-09 19:49:09 +0300

    update main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,19 @@
-import { ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
 
   app.enableCors({
-    origin: ["http://localhost:3000"], // фронт
-    credentials: true,
+    origin: [
+      'https://vkp-table-production.up.railway.app', // фронт на Railway
+      'http://localhost:3000',                        // для локальной разработки
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
+
 bootstrap();
+
